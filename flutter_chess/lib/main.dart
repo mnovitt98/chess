61ad555cd15600da62e1d2407d25f08386506a29
Matthew Novitt <matthew.novitt@gmail.com>
2024-07-18 20:12:13 -0400

    Full Chess Board Initialized with index echoing on squre tap.

--- flutter_chess/lib/main.dart
+++ flutter_chess/lib/main.dart
@@ -3,21 +3,74 @@ import 'dart:io';
 
 enum Piece { pawn, rook, knight, bishop, queen, king }
 enum PieceColor { white, black }
+
+/* these should be the values of the above enums... */
 const BlackSquareColor = Colors.grey;
 const WhiteSquareColor = Colors.white;
 
-final Map<Piece, (Image, Image)> pieceImgs = {
-  Piece.pawn:   (Image.asset("images/Chess_plt60.png"), Image.asset("images/Chess_pdt60.png")),
-  Piece.rook:   (Image.asset("images/Chess_rlt60.png"), Image.asset("images/Chess_rdt60.png")),
-  Piece.knight: (Image.asset("images/Chess_nlt60.png"), Image.asset("images/Chess_ndt60.png")),
-  Piece.bishop: (Image.asset("images/Chess_blt60.png"), Image.asset("images/Chess_bdt60.png")),
-  Piece.queen:  (Image.asset("images/Chess_qlt60.png"), Image.asset("images/Chess_qdt60.png")),
-  Piece.king:   (Image.asset("images/Chess_klt60.png"), Image.asset("images/Chess_kdt60.png"))
+/* as well as this...*/
+
+PieceColor getPCFromColor(Color c) {
+  return c == BlackSquareColor ? PieceColor.black : PieceColor.white;
+}
+
+final Map<Piece, Map<PieceColor, Image?>> pieceImgs = {
+  Piece.pawn: {
+    PieceColor.white : Image.asset("images/Chess_plt60.png"),
+    PieceColor.black : Image.asset("images/Chess_pdt60.png")
+  },
+  Piece.rook: {
+    PieceColor.white : Image.asset("images/Chess_rlt60.png"),
+    PieceColor.black : Image.asset("images/Chess_rdt60.png")
+  },
+  Piece.knight: {
+    PieceColor.white : Image.asset("images/Chess_nlt60.png"),
+    PieceColor.black : Image.asset("images/Chess_ndt60.png")
+  },
+  Piece.bishop: {
+    PieceColor.white : Image.asset("images/Chess_blt60.png"),
+    PieceColor.black : Image.asset("images/Chess_bdt60.png")
+  },
+  Piece.queen: {
+    PieceColor.white : Image.asset("images/Chess_qlt60.png"),
+    PieceColor.black : Image.asset("images/Chess_qdt60.png")
+  },
+  Piece.king: {
+    PieceColor.white : Image.asset("images/Chess_klt60.png"),
+    PieceColor.black : Image.asset("images/Chess_kdt60.png")
+  }
 };
 
-/* Should not move past this if any of the above images fail to load - do a try execpt and close
-   gracefully should they fail to load...
+/* Should not move past this if any of the above images fail to load -
+   do a try execpt and close gracefully should they fail to load...
 */
+
+List<List<Piece?>> ChessBoardRows = [
+  [
+    Piece.rook, Piece.knight, Piece.bishop, Piece.king,
+    Piece.queen, Piece.bishop, Piece.knight, Piece.rook,
+  ],
+  [
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+  ],
+  List<Piece?>.filled(8, null),
+  List<Piece?>.filled(8, null),
+  List<Piece?>.filled(8, null),
+  List<Piece?>.filled(8, null),
+  [
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+  ],
+  [
+    Piece.rook, Piece.knight, Piece.bishop, Piece.queen,
+    Piece.king, Piece.bishop, Piece.knight, Piece.rook,
+  ],
+];
+
+
+/* have a global event history for now. */
+
 
 (int, int) getMatrixIndex(int c_index) {
   return ((c_index / 8).toInt(), (c_index % 8).toInt());
@@ -32,17 +85,37 @@ Color getTileColor(int c_index) {
   return (row % 2) == (col % 2) ? WhiteSquareColor : BlackSquareColor;
 }
 
-TableRow makeBoardRow(int index_offset) {
-  return TableRow(
-    children: List.generate(
-      8,
-      (int index) => BoardTile(
-           index: index + index_offset,
-           tileColor: getTileColor(index + index_offset),
-           pieceImg: pieceImgs[Piece.king]?.$2
-      ),
-      growable: false
-    )
+TableRow makeBoardRow(List<Piece?> pieces, int index_offset) {
+  int index = 0;
+  List<BoardTile> bts = [];
+  for (Piece? p in pieces) {
+    Color tc = getTileColor(index + index_offset);
+    bts.add(BoardTile(
+      index: index + index_offset,
+      tileColor: tc,
+      pieceImg: pieceImgs[p]?[index_offset < 15 ? PieceColor.black : PieceColor.white],
+    ));
+    index++;
+  }
+
+  return TableRow(children: bts);
+}
+
+List<TableRow> makeBoardRows() {
+  List<TableRow> trs = [];
+  int index_offset = 0;
+  for (List<Piece?> r in ChessBoardRows) {
+    trs.add(makeBoardRow(r, index_offset));
+    index_offset += 8;
+  }
+
+  return trs;
+}
+
+Table makeChessBoard() {
+  return Table(
+    defaultColumnWidth: FixedColumnWidth(75),
+    children: makeBoardRows(),
   );
 }
 
@@ -63,30 +136,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(title),
         ),
-        body: Center(
-          child: Table(
-            columnWidths: const <int, TableColumnWidth>{
-              0: FixedColumnWidth(75),
-              1: FixedColumnWidth(75),
-              2: FixedColumnWidth(75),
-              3: FixedColumnWidth(75),
-              4: FixedColumnWidth(75),
-              5: FixedColumnWidth(75),
-              6: FixedColumnWidth(75),
-              7: FixedColumnWidth(75)
-            },
-            children: <TableRow>[
-              makeBoardRow(0),
-              makeBoardRow(8),
-              makeBoardRow(16),
-              makeBoardRow(24),
-              makeBoardRow(32),
-              makeBoardRow(40),
-              makeBoardRow(48),
-              makeBoardRow(56),
-            ]
-          )
-      )
+        body: Center(child: makeChessBoard())
     ));
   }
 }
@@ -111,4 +161,32 @@ class BoardTile extends StatelessWidget {
           )
         );
       }
+}
+
+class ChessBoardState extends ChangeNotifier {
+  final List<Piece?> _pieces = [
+    Piece.rook, Piece.knight, Piece.bishop, Piece.king,
+    Piece.queen, Piece.bishop, Piece.knight, Piece.rook,
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+    Piece.pawn, Piece.pawn, Piece.pawn, Piece.pawn,
+    Piece.rook, Piece.knight, Piece.bishop, Piece.queen,
+    Piece.king, Piece.bishop, Piece.knight, Piece.rook,
+  ];
+
+  Piece? getPieceAt(index) {
+    /* do bounds check */
+    return _pieces[index];
+  }
+
+  void update() {
+    notifyListeners();
+  }
 }
