d924f571d8b0aa1fd2e11435b9d60738eae3969e
Matthew Novitt <matthew.novitt@gmail.com>
2024-07-27 22:10:40 -0400

    Refactor websocket server, splitting off http parsing from the websocket connection negotiation and data frame marshalling.

---
+++ ChessEngine/src/WebSocket/HTTPParser.java
@@ -0,0 +1,54 @@
+package websocket;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.nio.ByteBuffer;
+
+class HTTPParser {
+    private static String LINE_SEP = "\r\n";
+
+    public static Map<String, String> parseRequestStartLine(String req) {
+        /* What we're after: GET / HTTP/1.1 */
+        String[] requestLine = req.split(HTTPParser.LINE_SEP)[0].split(" ");
+
+        Map<String, String> requestLineComponents = new HashMap();
+        requestLineComponents.put("method", requestLine[0]);
+        requestLineComponents.put("resource", requestLine[1]);
+        requestLineComponents.put("version", requestLine[2]);
+
+        return requestLineComponents;
+    }
+
+    public static Map<String, String> parseRequestHeaders(String req) {
+        int i = 0;
+        Map<String, String> requestHeaders = new HashMap();
+        for (String line : req.split("\r\n")) {
+            if (i++ == 0) { /* dont need the request start line */
+                continue;
+            }
+            String[] kv = line.split(": ");
+            if (kv.length == 1) { /* i.e. there was no match, happens at the */
+                break;            /* end of the list of headers...           */
+            }
+            requestHeaders.put(kv[0].toLowerCase(), kv[1]);
+        }
+        return requestHeaders;
+    }
+
+    public static int packHTTPResponse(String[] lines, int nlines, byte[] buf) {
+        ByteBuffer bbuf = ByteBuffer.wrap(buf);
+        int totalBytes = 0;
+        for (int i = 0; i < nlines; i++) {
+            byte[] lineWithSuffix = (lines[i] + HTTPParser.LINE_SEP).getBytes();
+            bbuf.put(lineWithSuffix);
+            totalBytes += lineWithSuffix.length;
+        }
+
+        // Need extra "\r\n" to separate headers from body.
+        byte[] suffix = HTTPParser.LINE_SEP.getBytes();
+        bbuf.put(suffix);
+        totalBytes += suffix.length;
+
+        return totalBytes;
+    }
+}
--- ChessEngine/src/WebSocket/WebSocketServer.java
+++ ChessEngine/src/WebSocket/WebSocketServer.java
@@ -2,101 +2,101 @@ package websocket;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Base64;
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-
 import java.nio.ByteBuffer;
-
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import websocket.HTTPParser;
 
+/**
+ * Establish and maintain a websocket connection with a client as defined by:
+ * <a href="https://datatracker.ietf.org/doc/html/rfc6455">
+ *   Request for Comments: 6455
+ * </a>.
+ * At time of writing, this class is not in full observance of the protocol; its
+ * functionality goes as far as to establish a connection with a client, and to
+ * send and receive data frames with small text payloads. Moreover, it limits
+ * itself to dealing with one client at a time. These limitations will be
+ * overcome, it is hoped, in due time.
+ */
 public class WebSocketServer {
-
     private static final int MAX_REQRESP_BUF_SIZ = 1024;
+    private static final String ACCEPT_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-    private ServerSocket ss;
-    private Socket client;
-    public InputStream is;
-    private OutputStream os;
+    /**
+     * Enumerate all possible opcodes for frame types. See:
+     * <a href="">Section 5.2: Base Framing Protocol</a>
+     */
+    private static enum Opcode {
+        CONTINUTATION ((byte) 0x0),
+        TEXT          ((byte) 0x1),
+        BINARY        ((byte) 0x2),
+        // %x3-7 are reserved for further non-control frames
+        CONN_CLOSE    ((byte) 0x8),
+        PING          ((byte) 0x9),
+        PONG          ((byte) 0xa);
+        // %xB-F are reserved for further control frames
 
-    /* parsing request, no real error handling done here in the interest of
-       time */
+        private byte value;
 
-    private static HashMap<String, String> parseRequestStartLine(String req) {
-        /* What we're after: GET / HTTP/1.1 */
-        String[] requestLine = req.split("\r\n")[0].split(" ");
-
-        HashMap<String, String> requestLineInfo = new HashMap();
-        requestLineInfo.put("method", requestLine[0]);
-        requestLineInfo.put("resource", requestLine[1]);
-        requestLineInfo.put("version", requestLine[2]);
-
-        return requestLineInfo;
-    }
-
-    private static HashMap<String, String> parseRequestHeaders(String req) {
-        int i = 0;
-        HashMap<String, String> requestHeaders = new HashMap();
-        for (String line : req.split("\r\n")) {
-            if (i++ == 0) { /* dont need the request start line */
-                continue;
-            }
-            String[] kv = line.split(": ");
-            if (kv.length == 1) { /* i.e. there was no match, happens at the */
-                break;            /* end of the list of headers...           */
-            }
-            requestHeaders.put(kv[0].toLowerCase(), kv[1]);
+        Opcode(byte val) {
+            this.value = val;
         }
-        return requestHeaders;
     }
 
-    /* some helper methods */
-
+    /**
+     * Determine the unsigned value stored in the byte argument.
+     * Since Java trades only in two's complement values, one must coerce a
+     * negative byte value into an int to get at its unsigned contents. This
+     * is done by taking the following sum:
+     * <p>
+     *   1111111111111111111111111xxxxxxx
+     * + 00000000000000000000000010000000
+     * ----------------------------------
+     *   0000000000000000000000000xxxxxxx
+     * </p>
+     * The overflow is discarded of course. Again, please note this is only
+     * performed when the byte is negative. Otherwise the value is the same
+     * signed or unsigned. This function is necessary when working with bytes
+     * off the wire.
+     *
+     * @param  b signed byte
+     * @return a positive int
+     */
     private static int convertByte(byte b) {
-        /* java doesnt have unsigned types, truly despicable */
-        if ((int) b < 0) {
-            return b + 128; /* 10000000 */
-        }
-        return b;
+        // Here a widening primitive conversion is performed: b gets promoted to
+        // the int value shown above before being added to 128.
+        return b < 0 ? b + 128 : b;
     }
 
-    /* put rfc link here */
+    /**
+     * Produce the Sec-WebSocket-Accept header value as defined in:
+     * <a href="https://datatracker.ietf.org/doc/html/rfc6455#section-4.2.2">
+     *  Section 4.2.2: Sending the Server's Opening Handshake
+     * </a>
+     * <p></p>
+     * Specifically, see step 5.4.
+     *
+     * @params key the Sec-WebSocket-Key header value sent by the client
+     */
     private static String getAcceptHeader(String key) {
-        /* note random constant */
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
-            md.update((key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes());
-            String accept = Base64.getEncoder().encodeToString(md.digest());
-            return accept;
+            md.update((key + WebSocketServer.ACCEPT_GUID).getBytes());
+            return Base64.getEncoder().encodeToString(md.digest());
         } catch (NoSuchAlgorithmException e) {
-            ;
+            System.out.println(e);
         }
         return "";
-    }
-
-    private static int packResponse(String[] lines, int nlines, byte[] buf) {
-        int nBytesSuffix = "\r\n".getBytes().length;
-
-        int totalBytes = 0;
-        ByteBuffer bbuf = ByteBuffer.wrap(buf);
-        for (int i = 0; i < nlines; i++) {
-            int nBytesLine = lines[i].getBytes().length;
-            bbuf.put((lines[i] + "\r\n").getBytes());
-            totalBytes += nBytesLine + nBytesSuffix;
-        }
-        bbuf.put("\r\n".getBytes());
-        totalBytes += nBytesSuffix;
-
-        return totalBytes;
     }
 
     private static String parseDataFrame(byte[] f) {
@@ -110,7 +110,7 @@ public class WebSocketServer {
             System.out.println("Larger payload.");
         } else if (payloadLength == 127) {
             /* adjust payloadLength. this is not currently handled */
-            maskOffset += 8; /* three bytes follow */
+            maskOffset += 8; /* eight bytes follow */
             System.out.println("Larger payload.");
         }
         int payloadOffset = maskOffset + 4;
@@ -141,6 +141,10 @@ public class WebSocketServer {
         return bs.toByteArray();
     }
 
+    private ServerSocket ss;
+    private Socket client;
+    private InputStream is;
+    private OutputStream os;
 
     public WebSocketServer(int port) throws IOException {
         this.ss = new ServerSocket(port);
@@ -155,12 +159,11 @@ public class WebSocketServer {
             System.out.println(String.format("Client %s accepted; upgrading connection to websocket.",
                                              client.getRemoteSocketAddress()));
 
-            /* this is blocking. should check this request for validity. again
-               in the interest of time, this will be defered. */
+            // This is blocking.
             byte[] mesg = new byte[MAX_REQRESP_BUF_SIZ];
             is.read(mesg);
 
-            upgradeConnection(WebSocketServer.parseRequestHeaders(new String(mesg, "ascii")));
+            upgradeConnection(HTTPParser.parseRequestHeaders(new String(mesg, "ascii")));
             System.out.println("Connection successfully upgraded.");
         } catch (Exception e) {
             System.out.println(e);
@@ -169,7 +172,7 @@ public class WebSocketServer {
     }
 
     /* link rfc */
-    private void upgradeConnection(HashMap<String, String> reqHeaders) {
+    private void upgradeConnection(Map<String, String> reqHeaders) {
         int numRespLines = 4;
         String[] respLines = new String[numRespLines];
         respLines[0] = "HTTP/1.1 101 Switching Protocols";
@@ -181,7 +184,7 @@ public class WebSocketServer {
         );
 
         byte[] resp = new byte[MAX_REQRESP_BUF_SIZ];
-        int respBytes = packResponse(respLines, numRespLines, resp);
+        int respBytes = HTTPParser.packHTTPResponse(respLines, numRespLines, resp);
 
         try {
             this.os.write(resp, 0, respBytes);
@@ -190,7 +193,7 @@ public class WebSocketServer {
         }
     }
 
-    /* this will block */
+    /* The following methods all block... */
     public void sendPing() {
         ByteArrayOutputStream bs = new ByteArrayOutputStream();
         bs.write((byte) 0x89);
@@ -202,7 +205,7 @@ public class WebSocketServer {
         }
     }
 
-    /* this will block */
+
     public void sendMesg(String mesg) {
         try {
             this.os.write(populateDataFrame(mesg));
@@ -211,7 +214,6 @@ public class WebSocketServer {
         }
     }
 
-    /* this will block */
     public String readMesg() {
         int bytes;
         byte[] mesg = new byte[MAX_REQRESP_BUF_SIZ];
