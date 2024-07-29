383eb2340da5c7812b7df3c1a2414208ece480ed
Matthew Novitt <matthew.novitt@gmail.com>
2024-07-29 01:23:09 -0400

    Queen working

--- ChessEngine/src/Chess/Pieces/Queen.java
+++ ChessEngine/src/Chess/Pieces/Queen.java
@@ -11,7 +11,35 @@ public class Queen extends Piece {
     }
 
     public MoveType isValidMove(Board b, Index src, Index dest) {
-        return MoveType.INVALID;
+        if (this.isLight()) {
+            src = new Index(src, this);
+        }
+
+        MoveType mt = MoveType.INVALID;
+        if (src.outOfBounds() || dest.outOfBounds()) {
+            return mt;
+        }
+
+        boolean attacking = b.pieceAt(dest);
+        if (attacking && Piece.sameColor(this, b.getPieceAt(dest))) {
+            System.out.println("Invalid: Bishop can't capture a piece of the same color.");
+            return MoveType.INVALID;
+        }
+
+        // should try to reuse rook and bishop code here, same with king
+        if (((b.openWalk(src, dest, Index.Direction.BACKWARD))
+            || (b.openWalk(src, dest, Index.Direction.FORWARD))
+            || (b.openWalk(src, dest, Index.Direction.LEFT))
+            || (b.openWalk(src, dest, Index.Direction.RIGHT)))
+            || (b.openWalk(src, dest, Index.Direction.QUADRANT_I))
+            || (b.openWalk(src, dest, Index.Direction.QUADRANT_II))
+            || (b.openWalk(src, dest, Index.Direction.QUADRANT_III))
+            || (b.openWalk(src, dest, Index.Direction.QUADRANT_IV))) {
+            System.out.println(String.format("Queen %s %s", attacking ? "takes" : "to", dest.inChessNotation()));
+            mt = attacking ? MoveType.CAPTURE : MoveType.ADVANCE;
+        }
+
+        return mt;
     }
 
     public String toString() {
