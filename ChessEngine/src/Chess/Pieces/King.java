42f6a2c1468547afa45d6234a693f60622e2c083
Matthew Novitt <matthew.novitt@gmail.com>
2024-07-29 01:29:17 -0400

    King working. Now that all pieces are working, move to factor out common code between them

--- ChessEngine/src/Chess/Pieces/King.java
+++ ChessEngine/src/Chess/Pieces/King.java
@@ -11,7 +11,35 @@ public class King extends Piece {
     }
 
     public MoveType isValidMove(Board b, Index src, Index dest) {
-        return MoveType.INVALID;
+        if (this.isLight()) {
+            src = new Index(src, this);
+        }
+
+        MoveType mt = MoveType.INVALID;
+        if (src.outOfBounds() || dest.outOfBounds()) {
+            return mt;
+        }
+
+        boolean attacking = b.pieceAt(dest);
+        if (attacking && Piece.sameColor(this, b.getPieceAt(dest))) {
+            System.out.println("Invalid: Bishop can't capture a piece of the same color.");
+            return MoveType.INVALID;
+        }
+
+        // should try to reuse rook and bishop code here, same with king
+        if (src.forward(1).equals(dest)
+            || src.backward(1).equals(dest)
+            || src.left(1).equals(dest)
+            || src.right(1).equals(dest)
+            || src.diagnol(1, Index.Direction.QUADRANT_I).equals(dest)
+            || src.diagnol(1, Index.Direction.QUADRANT_II).equals(dest)
+            || src.diagnol(1, Index.Direction.QUADRANT_III).equals(dest)
+            || src.diagnol(1, Index.Direction.QUADRANT_IV).equals(dest)){
+            System.out.println(String.format("King %s %s", attacking ? "takes" : "to", dest.inChessNotation()));
+            mt = attacking ? MoveType.CAPTURE : MoveType.ADVANCE;
+        }
+
+        return mt;
     }
 
     public String toString() {
